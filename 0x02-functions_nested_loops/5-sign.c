afa4a4065bae931a3d22335ef6a66c47d6abd26c
HamzaAbdullahi2020 <hamzatailor2020@gmail.com>
2023-06-20 15:32:56 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_sign - print sign
+ * @n: aurgument
+ * Return: 0
+ */
+int print_sign(int n)
+{
+	int test;
+
+	if (n > 0)
+	{
+		test = 1;
+		_putchar('+');
+	} else if (n == 0)
+	{
+		test = 0;
+		_putchar('0');
+	} else
+	{
+		test = -1;
+		_putchar('-');
+	}
+
+	return (test);
+}
