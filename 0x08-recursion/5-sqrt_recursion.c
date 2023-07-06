6cb17f25ca5698e7d081d5351a1169341db25c2f
HamzaAbdullahi2020 <hamzatailor2020@gmail.com>
2023-07-06 14:51:38 -0700

    5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * sqrt2 - function
+ * @a: parameter
+ * @b: parameter
+ * Return: 1
+ */
+int sqrt2(int a, int b)
+{
+	if (b * b == a)
+		return (b);
+	else if (b * b > a)
+		return (-1);
+	return (sqrt2(a, b + 1));
+}
+/**
+ * _sqrt_recursion - function
+ * @n: parameter
+ * Return: success
+ */
+int _sqrt_recursion(int n)
+{
+	return (sqrt2(n, 1));
+}
